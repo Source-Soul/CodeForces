95681e6a88de14fdfae2946b3488b54e0fee1da6
PriAurin <146650226+PriAurin@users.noreply.github.com>
2023-11-15 18:24:31 +0600

    Add files via upload

---
+++ Source_code/496A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,f,p,q,arr[200],count=0;
+    cin>>f>>p;
+    for(i=0;i<p;i++)
+    {
+        cin>>arr[i];
+    }
+    cin>>q;
+    for(i=p;i<p+q;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+(p+q));
+    for(i=0;i<(p+q);i++)
+    {
+        if(arr[i]!=arr[i+1])
+        {
+            count++;
+        }
+    }
+    if(count==f)
+        cout<<"I become the guy."<<endl;
+    else
+        cout<<"Oh, my keyboard!"<<endl;
+}
