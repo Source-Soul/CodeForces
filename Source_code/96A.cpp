6af379c3729aa15fdfefb9c7020fd668ab7ba323
PriAurin <146650226+PriAurin@users.noreply.github.com>
2023-11-30 21:47:58 +0600

    Add files via upload

---
+++ Source_code/96A.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int x=0,y=0,z=0;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]=='1')
+        {
+            x++;
+            y=0;
+        }
+        else
+        {
+            y++;
+            x=0;
+
+        }
+        if(x==7 || y==7)
+        {
+            z=1;
+        }
+    }
+    if(z==1)
+
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
